baf83e500c71b207205aa43d9a8ce9a180867cd3
Raunak <raunakgiri21@gmail.com>
2023-11-16 16:46:15 +0530

    Added Simple Info in HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/shared/Layout";
 import UserDetails from "./pages/UserDetails";
 import AccountCreation from "./pages/AccountCreation";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
@@ -11,6 +12,10 @@ export default function App() {
           path="/"
           element={<Layout />}
         >
+          <Route
+            index
+            element={<HomePage />}
+          />
           <Route
             path="user-details"
             element={<UserDetails />}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className="flex justify-center mt-10">
+      <p className="flex text-2xl text-slate-500 mx-4">
+        Please use navbar/menu to switch between tabs <p className='animate-bounce mt-1 ml-4'>🔝</p>
+      </p>
+    </div>
+  );
+}
+
+export default HomePage
